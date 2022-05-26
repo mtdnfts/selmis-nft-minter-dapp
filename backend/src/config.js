90dba157f02cf299e741978bf02f8009bcfa89dc
Tate Downs <mtd.nfts@gmail.com>
2022-05-26 15:04:56 -0500

    website update

--- backend/src/config.js
+++ backend/src/config.js
@@ -32,7 +32,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://selmisnfts.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://selmisnfts.com", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
